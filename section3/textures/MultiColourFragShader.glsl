cf850b7129e2fc607928d421641ceb55789d03f2
Dan Hutchinson <d.hutchinson4@googlemail.com>
2018-01-12 13:39:24 +0000

    Texture tutorial

---
+++ section3/textures/MultiColourFragShader.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 color;
+
+uniform vec4 ourColor;
+in vec4 vertexColor; // this is linked to the output from above..
+in vec2 texCoords;
+
+uniform sampler2D ourTexture;A
+uniform sampler2D ourTexture2;
+
+void main()
+{
+    color = texture(ourTexture, texCoords) * vec4(vertexColor);
+}
---
+++ section3/textures/main.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <cmath>
+
+// OpenGL Extension Manager
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include "stb_image.h"
+
+#include "Shader.h"
+
+const GLint WIDTH = 800;
+const GLint HEIGHT = 600;
+
+unsigned char* loadTexture(const char* filePath, int* width, int* height, int* numChannel)
+{
+    return stbi_load(filePath, width, height, numChannel, 0);
+}
+
+int main(int argc, const char** argv)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
+    if(window == nullptr)
+    {
+        std::cerr << "Failed to create window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // This returns the actual underlying DPI
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+    glfwMakeContextCurrent(window);
+
+    // Allow us to use newer OpenGL features.
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialise OpenGL Extension Wrangler (GLEW)" << std::endl;
+        return -1;
+    }
+
+    // Setup OpenGL viewport
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    // Load the texture.
+    int width, height, numChannels;
+    unsigned char* data = loadTexture("container.jpg", &width, &height, &numChannels);
+    if(!data)
+    {
+        std::cerr << "Unable to load texture.";
+        return 1;
+    }
+    // Creating an OpenGL texture resource.
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    // Set it as the target for our subsequent calls.
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Set up texture wrapping and filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Now we're going to transfer that texture data previously loaded to the CPU
+    glTexImage2D(GL_TEXTURE_2D,     // target
+                 0,                 // mip map level
+                 GL_RGB,            // format we'll store the pixels in OpenGL
+                 width, height,     // texture width & height
+                 0,                 // legacy
+                 GL_RGB,            // source image format
+                 GL_UNSIGNED_BYTE,  // data type of the source image.
+                 data);             // pointer to image data.
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // Free the image memory in RAM (its now on the GPU)
+    stbi_image_free(data);
+
+    // Setup the shaders
+    gl::Shader multiColorShader("SimpleVShader.glsl", "MultiColourFragShader.glsl");
+
+    GLfloat verticies[] = {
+        // positions          // colours       // texture coordinates
+        0.5f,   0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   1.0f, 1.0f,         // top right
+        0.5f,  -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,         // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,         // bottom left
+        -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 1.0f          // top left
+    };
+
+    GLint indicies[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // Create Vertex Buffer Objects one to store each of our triangles.
+    // The VBO actually stores our data the VAO stores the vertex attribute
+    // configuration that's applied to it.
+    GLuint vbo;
+    GLuint ebo;
+    GLuint vao;
+
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    glGenVertexArrays(1, &vao);
+
+    // Load the vertex buffer and store its configuration in the vertex array
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+    // Copy data to the GPU
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+    // Configure the vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), /* stride */ (GLvoid*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), /* stride */ (GLvoid*)(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), /* stride */ (GLvoid*)(6 * sizeof(GLfloat)));
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+    // Unbind this vertex array.
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Unbind the VBO and VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Set up the game loop...
+    while( !glfwWindowShouldClose(window) )
+    {
+        glfwPollEvents();
+
+        // Render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+
+        // Load the configuration stored in the vertex array.
+        multiColorShader.use();
+
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+
+        // Unbind the array...
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+    }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &ebo);
+    glDeleteBuffers(1, &vbo);
+
+    glfwTerminate();
+    return 0;
+}
