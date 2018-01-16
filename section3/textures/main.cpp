4a77e9b19855de009e415a2eb19a68090ea465a4
Dan Hutchinson <daniel.hutchinson@foundry.com>
2018-01-16 09:27:09 +0000

    change to GL_NEAREST slightly more pixelated

--- section3/textures/main.cpp
+++ section3/textures/main.cpp
@@ -43,8 +43,8 @@ void configureTexture(const char* filePath, GLuint* textureID)
     // Set up texture wrapping and filtering
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     // Now we're going to transfer that texture data previously loaded to the CPU
     glTexImage2D(GL_TEXTURE_2D,     // target
